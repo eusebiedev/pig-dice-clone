c630c6b626189546c7b8a6c07fa42c71c4d08778
Eusebie Siebenberg <siebenee@gmail.com>
2023-04-11 11:30:33 -0700

    Add initial test to test for Add player function to dice.test.js

--- __tests__/dice.test.js
+++ __tests__/dice.test.js
@@ -1,4 +1,4 @@
-import { Game } from './../src/dice.js';
+import { Game, Player } from './../src/dice.js';
 
 describe('Game', () => {
 
@@ -8,4 +8,15 @@ describe('Game', () => {
     expect(game.currentId).toEqual(1);
     expect(game.maxId).toEqual(0);
   });
+});
+
+describe('Player', () => {
+
+  test('should create a new player with initialized scores', () => {
+    const player = new Player();
+    expect(player.playerName).toEqual([]);
+    // expect(player.totalScore).toEqual(0);
+    // expect(player.roundScore).toEqual(0);
+    // expect(player.roll).toEqual(0);
+  })
 });
