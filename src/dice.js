c9e2b1a2356617ab3ba406dcf1f3cbc4ead15b70
Eusebie Siebenberg <siebenee@gmail.com>
2023-04-11 11:19:43 -0700

    Add object properties to Game, Test successfully created a new initialized Game object

--- src/dice.js
+++ src/dice.js
@@ -6,61 +6,61 @@ export function Game(){
   this.maxId = 0;
 }
 
-Game.prototype.addPlayer = function(player){
-  player.id = this.assignId();
-  this.gamePlayers[player.id] = player;
-};
+// Game.prototype.addPlayer = function(player){
+//   player.id = this.assignId();
+//   this.gamePlayers[player.id] = player;
+// };
 
-Game.prototype.assignId = function(){
-  this.maxId += 1;
-  return this.maxId;
-};
+// Game.prototype.assignId = function(){
+//   this.maxId += 1;
+//   return this.maxId;
+// };
 
-Game.prototype.findPlayer = function(){
-  let currentPlayer;
-  Object.keys(this.gamePlayers).forEach(function(key) {
-    if(Game.currentId.toString() ===  key){
-      currentPlayer = this.gamePlayers[key];
-    }
-  });
-  return currentPlayer;
-};
+// Game.prototype.findPlayer = function(){
+//   let currentPlayer;
+//   Object.keys(this.gamePlayers).forEach(function(key) {
+//     if(Game.currentId.toString() ===  key){
+//       currentPlayer = this.gamePlayers[key];
+//     }
+//   });
+//   return currentPlayer;
+// };
 
-Game.prototype.updateCurrentId = function(){
-  if(this.currentId >= this.maxId){
-    this.currentId = 1;
-  } else {
-    this.currentId += 1;
-  }
-};
+// Game.prototype.updateCurrentId = function(){
+//   if(this.currentId >= this.maxId){
+//     this.currentId = 1;
+//   } else {
+//     this.currentId += 1;
+//   }
+// };
 
-//Business Logic for Players
-export function Player(playerName) {
-  this.playerName = playerName;
-  this.totalScore = 0;  
-  this.roundScore = 0;
-  this.roll = 0;
-}
+// //Business Logic for Players
+// export function Player(playerName) {
+//   this.playerName = playerName;
+//   this.totalScore = 0;  
+//   this.roundScore = 0;
+//   this.roll = 0;
+// }
 
-//Business Logic for Player Turns
+// //Business Logic for Player Turns
 
-Player.prototype.playerTurn = function() {
-  this.rollDice();
-  this.roundScore += this.roll;
-};
+// Player.prototype.playerTurn = function() {
+//   this.rollDice();
+//   this.roundScore += this.roll;
+// };
 
-Player.prototype.rollDice = function() {
-  this.roll = Math.ceil(Math.random()*6); 
-};
+// Player.prototype.rollDice = function() {
+//   this.roll = Math.ceil(Math.random()*6); 
+// };
 
-Player.prototype.checkWin = function(){
-  if(this.roundScore + this.totalScore >= 100){
-    alert("This player has won the game");
-  }
-};
+// Player.prototype.checkWin = function(){
+//   if(this.roundScore + this.totalScore >= 100){
+//     alert("This player has won the game");
+//   }
+// };
 
-Player.prototype.holdDice = function(){
-  this.totalScore += this.roundScore;
-  this.roundScore = 0;
-  this.roll = 0;
-};
+// Player.prototype.holdDice = function(){
+//   this.totalScore += this.roundScore;
+//   this.roundScore = 0;
+//   this.roll = 0;
+// };
